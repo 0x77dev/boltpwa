fb27df64cdf6b1e2c2656a3a26cf325225ecbb0a
Misha Marinenko <0x77dev@protonmail.com>
2019-10-08 18:12:56 +0300

    BASE

---
+++ src/components/Bolt.vue
@@ -0,0 +1,18 @@
+<template>
+	<iframe src="https://appetize.io/embed/cnh02d4hmfu2j4wzuzp5tgnur8?device=nexus5&scale=100&autoplay=true&orientation=portrait&deviceColor=black&screenOnly=true" width="360px" height="640px" frameborder="0" scrolling="no"></iframe>
+</template>
+
+<script>
+export default {
+  name: 'BoltEmulator',
+}
+</script>
+
+<style>
+	iframe{
+		position: absolute;
+		left: 50%;
+		top: 50%;
+		transform: translate(-50%, -50%);
+	}
+</style>
