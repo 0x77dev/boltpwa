bf35efdb73d6a24e46283f749a121d0f57f114dc
Misha Marinenko <0x77dev@protonmail.com>
2019-10-08 18:14:26 +0300

    compilted to docs

---
+++ docs/precache-manifest.a706b67418e4c29ea6eab1302fb1afdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7dd9900a22ad7077e754",
+    "url": "/css/app.0be05e3d.css"
+  },
+  {
+    "revision": "7dd9900a22ad7077e754",
+    "url": "/js/app.4a9d9e11.js"
+  },
+  {
+    "revision": "00e470d8e9fd106f1efe",
+    "url": "/js/chunk-vendors.bcbdbe80.js"
+  },
+  {
+    "revision": "22ae52d9cc926c38158a7a80f7e435a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
